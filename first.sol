78cc908ea86590f59ee6515117088e29ccaefea1
Sourov2002t <143200868+Sourov2002t@users.noreply.github.com>
2024-06-06 09:59:01 +0600

    challange1.sol

--- first.sol
+++ first.sol
@@ -1,24 +1,56 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity 0.8.9;
 
-contract sum{
-    uint a; //state variables
-    uint b; //state variables
-    address c; //state variables
+contract SimpleContract {
+    // Declare a boolean variable
+    bool public isActive;
 
-constructor(uint a1,uint a2){
-a=a1;
-b=a2;
-}
-function result() private view returns(uint k){  
- return a+b;
-}
-function res() internal view returns(uint){ 
-    uint d=result(); //local variable
-    return d;
-}
-function r() public payable  returns(uint){ 
-uint amount =msg.value; //Global variable
-return amount;
-}
+    // Declare an unsigned integer variable
+    uint public UID;
+
+    // Declare a string variable
+    string public name;
+
+    // Declare an address variable
+    address public add_owner;
+
+    // Set the value of the boolean variable
+    function setActive(bool Active) public {
+        isActive = Active;
+    }
+
+    // Get the value of the boolean variable
+    function getActive() public view returns (bool) {
+        return isActive;
+    }
+
+    // Set the value of the unsigned integer variable0
+    function setuid(uint Std_UID) public {
+        UID = Std_UID;
+    }
+
+    // Get the value of the unsigned integer variable
+    function getuid() public view returns (uint) {
+        return UID;
+    }
+
+    // Set the value of the string variable
+    function setName(string memory _name) public {
+        name = _name;
+    }
+
+    // Get the value of the string variable
+    function getName() public view returns (string memory) {
+        return name;
+    }
+
+    // Set the value of the address variable
+    function setOwner(address _owner) public {
+        add_owner = _owner;
+    }
+
+    // Get the value of the address variable
+    function getOwner() public view returns (address) {
+        return add_owner;
+    }
 }
