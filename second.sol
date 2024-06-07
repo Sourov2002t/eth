80e08f35f399b54b2b68f95a1f27a3e49839fe10
Sourov2002t <143200868+Sourov2002t@users.noreply.github.com>
2024-06-07 21:06:25 +0600

    second.sol

--- second.sol
+++ second.sol
@@ -1,1 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
 
+contract SmartData {
+    // Event to notify when ether is received with calculated values
+    event ReceivedEther(
+        address indexed sender,
+        uint256 weiAmount ,
+        uint256 gweiAmount,
+        uint256 etherAmount
+    );
+
+    // Function to receive ether and calculate its value
+    function receiveEther() public payable {
+        // Calculate the value of ether received in various denominations
+        uint256 weiAmount = msg.value;
+        uint256 gweiAmount = weiAmount / 1e9; 
+        uint256 etherAmount = weiAmount / 1e18;
+        
+        // Emit event to notify of ether received
+        emit ReceivedEther(msg.sender, weiAmount, gweiAmount, etherAmount);
+    }
+
+    // Fallback function to receive ether
+    function setBalance() public payable {
+        receiveEther();
+    }
+
+    // Function to retrieve the balance of the contract
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+}
